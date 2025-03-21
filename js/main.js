17182bf39fddd329d19432f5091c52cd40ccac02
Erka0007 <165401469+Erka0007@users.noreply.github.com>
2025-03-21 14:32:09 +0100

    skapade grid och fixade nav m.m

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-let layer = document.querySelector(".fade-layer");
-layer.addEventListener("click",showMenu)
-
 let button = document.querySelector(".menu-button");
 button.addEventListener("click",showMenu)
 
@@ -8,8 +5,5 @@ button.addEventListener("click",showMenu)
 function showMenu(){
     let menu = document.querySelector("nav.menu");
     menu.classList.toggle("show");
-
-    let layer = document.querySelector(".fade-layer");
-    layer.classList.toggle("visible"); 
    
 }
